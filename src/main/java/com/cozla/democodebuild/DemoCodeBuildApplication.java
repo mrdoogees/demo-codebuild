d4b6378b8311bee12de31cdd13f6bfa62c45d25c
Randrianalivelo Manohisoa <manohisoa.randrianalivelo-cic@ibm.com>
2022-05-18 11:11:39 +0200

    Init repo

---
+++ src/main/java/com/cozla/democodebuild/DemoCodeBuildApplication.java
@@ -0,0 +1,13 @@
+package com.cozla.democodebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCodeBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCodeBuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cozla/democodebuild/DemoCodeBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cozla.democodebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCodeBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
